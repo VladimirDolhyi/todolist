8484a503cae243fa8223cd4a4fd19a6aa47a2611
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-11-13 20:18:51 +0200

    add double quotes & flake8

--- notepad/apps.py
+++ notepad/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NotepadConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notepad'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "notepad"
